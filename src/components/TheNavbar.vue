694f0c4e455c27c05c53b38f66fa55a70c613911
Tair2033 <tairenikee@gmail.com>
2023-07-29 16:12:49 +0400

    prettier setup

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar-wrapper">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang='ts'>
+
+export default {
+  name: 'TheNavbar'
+}
+
+</script>
+
+<style lang='scss'></style>
